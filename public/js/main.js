f53721ed6a734358ddb1815e048deeb4ca0f0b12
Kevin Stubblefield <kevins12.92@gmail.com>
2017-05-08 23:12:20 -0600

    Add frackn project. Add scrolling

--- public/js/main.js
+++ public/js/main.js
@@ -11,3 +11,50 @@ $('#blog-link').on('click', function (event) {
         $(this).attr('href', '/blog/createPost');
     }
 });
+
+$('.dot').each(function (index, element) {
+    $(this).on('click', function (event) {
+        $('.dot').removeClass('active');
+        $(this).addClass('active');
+        $('.project.active').fadeOut(250, function () {
+            $(this).removeClass('active');
+            $('.project').eq(index).fadeIn(250).addClass('active');
+        });
+    });
+});
+
+var currentProject = 0;
+var projects = $('.project');
+var dots = $('.dot');
+
+$('#left-arrow').on('click', function (event) {
+    projects.eq(currentProject).fadeOut(250, function () {
+        projects.eq(currentProject).removeClass('active');
+        dots.eq(currentProject).removeClass('active');
+
+        if (currentProject == 0) {
+            currentProject = projects.length - 1;
+        } else {
+            currentProject--;
+        }
+
+        projects.eq(currentProject).fadeIn(250).addClass('active');
+        dots.eq(currentProject).addClass('active');
+    });
+});
+
+$('#right-arrow').on('click', function (event) {
+    projects.eq(currentProject).fadeOut(250, function () {
+        projects.eq(currentProject).removeClass('active');
+        dots.eq(currentProject).removeClass('active');
+
+        if (currentProject == projects.length -1) {
+            currentProject = 0;
+        } else {
+            currentProject++;
+        }
+
+        projects.eq(currentProject).fadeIn(250).addClass('active');
+        dots.eq(currentProject).addClass('active');
+    });
+});
