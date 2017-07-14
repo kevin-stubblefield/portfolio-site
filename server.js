be5f52d66c2a8166a80fc25f8713b9d1aa35b6a6
Kevin Stubblefield <kevins12.92@gmail.com>
2017-07-13 23:53:07 -0500

    Refactor httpsRedirect into utils.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var ect = require('ect');
 var ectRenderer = ect({ watch: true, root: __dirname + '/views/layouts', ext: '.ect' });
 var schedule = require('node-schedule');
 var cookieparser = require('cookie-parser');
+var utils = require('./utils.js');
 
 var indexRoutes = require('./routes/index.js');
 var blogRoutes = require('./routes/blog.js');
@@ -18,19 +19,7 @@ var PORT = process.env.PORT || 3000;
 app.set('view engine', 'ect');
 app.engine('ect', ectRenderer.render);
 
-var httpsRedirect = function(req, res, next) {
-    if (process.env.NODE_ENV === 'production') {
-        if (req.headers['x-forwarded-proto'] != 'https') {
-            return res.redirect('https://' + req.headers.host + req.url);
-        } else {
-            return next();
-        }
-    } else {
-        return next();
-    }
-};
-
-app.use(httpsRedirect);
+app.use(utils.httpsRedirect);
 app.use(cookieparser());
 app.use('/', indexRoutes);
 app.use('/blog', blogRoutes);
