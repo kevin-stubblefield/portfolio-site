dd220cdb1482cf8a90b93310ce9320eb096918bf
Kevin Stubblefield <kevins12.92@gmail.com>
2017-07-14 21:59:54 -0500

    Reorganize server.js

--- server.js
+++ server.js
@@ -4,27 +4,33 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
-var ect = require('ect');
-var ectRenderer = ect({ watch: true, root: __dirname + '/views/layouts', ext: '.ect' });
 var schedule = require('node-schedule');
 var cookieparser = require('cookie-parser');
 var utils = require('./utils.js');
 
-var indexRoutes = require('./routes/index.js');
-var blogRoutes = require('./routes/blog.js');
-var apiRoutes = require('./routes/api.js');
-
 var PORT = process.env.PORT || 3000;
+
+// View Engine config
+var ect = require('ect');
+var ectRenderer = ect({
+    watch: true,
+    root: __dirname + '/views/layouts',
+    ext: '.ect'
+});
 
 app.set('view engine', 'ect');
 app.engine('ect', ectRenderer.render);
+
+// Route and middleware config
+var indexRoutes = require('./routes/index.js');
+var blogRoutes = require('./routes/blog.js');
+var apiRoutes = require('./routes/api.js');
 
 app.use(utils.httpsRedirect);
 app.use(cookieparser());
 app.use('/', indexRoutes);
 app.use('/blog', blogRoutes);
 app.use('/api', apiRoutes);
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Catch-all 404
