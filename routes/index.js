6f1951d4410edeec81367584a7f0d444c0ecfdc7
Kevin Stubblefield <kevins12.92@gmail.com>
2017-09-03 15:30:47 -0700

    Add debug apk for testing and route to download

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 router.get('/', function (req, res) {
     res.render('index', {
@@ -11,6 +12,10 @@ router.get('/about', function (req, res) {
     res.render('about', {
         title: "About Me!"
     });
+});
+
+router.get('/hot-cold', function (req, res) {
+    res.sendFile(path.resolve('public/files/app-debug.apk'));
 });
 
 module.exports = router;
