b912c92ce6959da70743c682ca45f90b3ef0a3b1
Kevin Stubblefield <kevins12.92@gmail.com>
2018-03-13 17:58:38 -0700

    Begin work on bills ui

--- controllers/billController.js
+++ controllers/billController.js
@@ -3,6 +3,7 @@ let router = express.Router();
 let bodyParser = require('body-parser');
 let db = require('../db');
 let utils = require('../utils');
+let _ = require('lodash');
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({
@@ -12,7 +13,10 @@ router.use(bodyParser.urlencoded({
 router.get('/', utils.requireAuth(0), async function(req, res) {
     let bills = await db.getBills(req.user.id);
 
-    res.json(bills);
+    res.render('bills/home', {
+        title: _.capitalize(req.user.username) + '\'s Bills',
+        bills: bills
+    });
 });
 
 module.exports = router;
